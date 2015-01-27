8538afa36bb5543156dd2fe233043335b2431db8
Girish Subramanyam <girishsu@sleeplord-lm.corp.yahoo.com>
2015-01-26 16:28:17 -0800

    Initial Commit

---
+++ RottenTomatoes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RottenTomatoes
+//
+//  Created by Girish Subramanyam on 1/26/15.
+//  Copyright (c) 2015 girishsu@yahoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
